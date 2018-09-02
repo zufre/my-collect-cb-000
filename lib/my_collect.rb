c2afd5955f172b020006a6e45f1812031be4b5a6
Cedric David <cedric.david@btopenworld.com>
2018-09-02 12:45:38 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(array)
-  i = 0
   collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
+  i = 0
+  while i < array.length do
+     collection << yield(array[i])
+     i += 1
   end
   collection
+
 end
